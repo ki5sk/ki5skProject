a33d3d21dcd3afc7981b95e63290202c0c59c104
baerongmerong <baerongmerong@naver.com>
2023-12-29 12:14:34 +0900

    detail 수정

--- KI5SK/DetailOption/DetailOptionVC.swift
+++ KI5SK/DetailOption/DetailOptionVC.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class DetailOptionViewController: UIViewController {
 
+    
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        imView.image = UIImage(named: photo)
+//        imageView.image = img
+        
     }
 }
