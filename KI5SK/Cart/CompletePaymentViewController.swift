d445d485d18f7bcaa318eb091d5d93e5866a349b
dave17code <dave17code@gmail.com>
2023-12-31 16:23:39 +0900

    feature/CompletePaymentVC

---
+++ KI5SK/Cart/CompletePaymentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CompletePaymentViewController.swift
+//  KI5SK
+//
+//  Created by woonKim on 2023/12/31.
+//
+
+import UIKit
+import Gifu
+
+class CompletePaymentViewController: UIViewController {
+    
+    @IBOutlet weak var completePaymentGif: GIFImageView!
+    @IBOutlet weak var timerDismissLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        completePaymentGif.animate(withGIFNamed: "CompletePaymentGif") {}
+        fireTimer()
+    }
+    
+    func fireTimer() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.timerDismissLbl.text = "\(2)초 뒤 자동으로 닫힙니다."
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.timerDismissLbl.text = "\(1)초 뒤 자동으로 닫힙니다."
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+        }
+    }
+}
